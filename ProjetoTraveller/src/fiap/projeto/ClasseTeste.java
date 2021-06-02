42dde3738b843e606b39ed131663a8dda56f98db
ShimSooChang <rm88760@fiap.com.br>
2021-06-02 19:48:15 -0300

    instanciei mais objetos na classe teste

--- ProjetoTraveller/src/fiap/projeto/ClasseTeste.java
+++ ProjetoTraveller/src/fiap/projeto/ClasseTeste.java
@@ -4,9 +4,24 @@ public class ClasseTeste {
 
 	public static void main(String[] args) {
 		LocadoraVeiculos unidas = new LocadoraVeiculos();
+		unidas.setLocalizacao("R. Maria Prestes Maia, 745 - Vila Guilherme, São Paulo - SP, 02047-000");
+		unidas.setLinkCmChegar("shorturl.at/lnxOQ");
+		unidas.setNotaAvaliacao(4.5);
+		unidas.setLinkContato("https://www.unidas.com.br/");
+		unidas.setTelefoneContato(0+800121121);
+		unidas.setBreveDescricao("A Unidas é uma das maiores empresas especializada em aluguel de carros em todo o Brasil");
+		unidas.setNomeLocadora("Unidas");
+		unidas.setLinkImagem("https://www.mercadoeeventos.com.br/wp-content/uploads/2020/09/Unidas_Aluguel_de_Carros_RAC_13.jpg");
+		
 		// (String nomeCarro,String marca,String modeloCarroceria, String cor,int qtPortas,int ano,String linkFoto,String tipoCambio)
-		unidas.novoCarro("Argo", "Fiat", "Hatchback", "Preta", 4, 2020, "https://www.unidas.com.br/media/3757/fiat-argo.png", "Manual");
-		unidas.novoCarro("Rav4", "Fiat", "Hatchback", "Preta", 4, 2020, "https://www.unidas.com.br/media/3757/fiat-argo.png", "Manual");
+		unidas.novoCarro("Argo", "Fiat", "Hatchback", "Preta", 4, "https://www.unidas.com.br/media/3757/fiat-argo.png", "Manual");
+		unidas.novoCarro("Logan", "Renault", "Sedan", "Prata", 4, "https://www.unidas.com.br/media/3766/renault-logani.png", "Manual");
+		
+		unidas.printInformacoes();
+		
+		unidas.modelosDisponiveis();
+		
+		
 		
 		
 		
